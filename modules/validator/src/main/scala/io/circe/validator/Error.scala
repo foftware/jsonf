c6a1a574ef0a9d20ff9b71e142911f8fe5145815
Marek Kidon <marek.kidon@ceai.io>
2019-05-03 17:59:26 +0200

    Added Error equality

--- modules/validator/src/main/scala/io/circe/validator/Error.scala
+++ modules/validator/src/main/scala/io/circe/validator/Error.scala
@@ -1,20 +1,26 @@
 package io.circe.validator
 
-import cats.Monad
-import cats.syntax.flatMap._
+import cats.kernel.Eq
+import cats.{Monad, Show}
+// import cats.syntax.flatMap._
 import cats.data.Chain
+import cats.implicits._
 import cats.mtl.{ApplicativeLocal => AL, FunctorTell => FT}
 import io.circe.JsonObject
 
 final case class ErrorAt(at: Path, error: JsonError)
 
+object ErrorAt {
+  implicit val eqErrorAt: Eq[ErrorAt] = Eq.fromUniversalEquals
+}
+
 /** Sum of all possible validation errors */
 sealed trait JsonError
 
-object JsonError {
+object JsonError extends Instances {
 
-  final case class TypeMismatch[A, B](expected: A, got: B) extends JsonError
-  final case class PredicateViolation(reason: String)      extends JsonError
+  final case class TypeMismatch(expected: String, got: String) extends JsonError
+  final case class PredicateViolation(reason: String)          extends JsonError
   final case class ArrayPredicateViolation[A, B](message: String)
       extends JsonError
   final case class KeyNotFound(key: String, obj: JsonObject) extends JsonError
@@ -24,13 +30,13 @@ object JsonError {
   )(implicit FT: FT[F, Errors], L: AL[F, Env], M: Monad[F]): F[Unit] =
     L.reader(_.path) >>= (path => FT.tell(Chain.one(ErrorAt(path, e))))
 
-  def mismatch0[A, B](a: A, b: B): JsonError = TypeMismatch(a, b)
+  def mismatch0(a: String, b: String): JsonError = TypeMismatch(a, b)
 
-  def mismatch[F[_], A, B](
+  def mismatch[F[_], A: Show, B: Show](
       a: A,
       b: B
   )(implicit FT: FT[F, Errors], L: AL[F, Env], M: Monad[F]): F[Unit] =
-    errorAt(mismatch0(a, b))
+    errorAt(mismatch0(a.show, b.show))
 
   val predicateViolation0: String => JsonError = PredicateViolation.apply
 
@@ -47,4 +53,8 @@ object JsonError {
       b: JsonObject
   )(implicit FT: FT[F, Errors], L: AL[F, Env], M: Monad[F]): F[Unit] =
     errorAt(keyNotFound0(a, b))
+}
+
+trait Instances {
+  implicit val eqJsonError = Eq.fromUniversalEquals
 }
