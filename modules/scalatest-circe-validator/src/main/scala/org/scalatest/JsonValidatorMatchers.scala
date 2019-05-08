dc5a52624eba04e912ceb5e2f46bc9c9a8429cfb
fokot <frantisek.kocun@gmail.com>
2019-05-08 14:28:35 +0200

    matchJson  interpolator

--- modules/scalatest-circe-validator/src/main/scala/org/scalatest/JsonValidatorMatchers.scala
+++ modules/scalatest-circe-validator/src/main/scala/org/scalatest/JsonValidatorMatchers.scala
@@ -1,9 +1,13 @@
 package org.scalatest
 
-import io.circe.validator.{Validator, run => runValidator}
 import io.circe.Json
-import org.scalatest.matchers.{Matcher, MatchResult}
+import io.circe.validator.literal.ValidatorMacrosTrait
+import io.circe.validator.{Validator, run => runValidator}
 import org.scalatest.JsonValidatorPrettifier.jsonValidatorPrettifier
+import org.scalatest.matchers.{MatchResult, Matcher}
+
+import scala.language.experimental.macros
+import scala.reflect.macros.blackbox
 
 trait JsonValidatorMatchers {
 
@@ -22,4 +26,22 @@ trait JsonValidatorMatchers {
 
   def passValidation(validator: Validator): Matcher[Json] =
     new JsonPassesValidation(validator)
+
+  implicit class MatchJsonInterpolator(sc: StringContext) {
+    def matchJson(args: Any*): Matcher[Json] =
+      macro MatchJsonMacros.jsonMatcherStringContext
+  }
+}
+
+class MatchJsonMacros(val c: blackbox.Context)
+    extends ValidatorMacrosTrait
+    with JsonValidatorMatchers {
+  import c.universe._
+
+  final def jsonMatcherStringContext(
+      args: c.Expr[Any]*
+  ): c.Expr[Matcher[Json]] =
+    c.Expr[Matcher[Json]](
+      q"passValidation(${validatorStringContextTree(args: _*)})"
+    )
 }
--- modules/scalatest-circe-validator/src/test/scala/org/scalatest/JsonValidatorMarchersSpec.scala
+++ modules/scalatest-circe-validator/src/test/scala/org/scalatest/JsonValidatorMarchersSpec.scala
@@ -2,7 +2,7 @@ package org.scalatest
 
 import io.circe.Json
 import io.circe.validator.trueValidator
-// import org.scalatest.matchers.MatchResult
+import io.circe.literal._
 
 class JsonValidatorMatchersSpec
     extends Matchers
@@ -28,6 +28,23 @@ class JsonValidatorMatchersSpec
 
         actual.matches shouldBe false
       }
+    }
+  }
+
+  "matchJson" should {
+
+    "succeed" in {
+      json"""{ "a": true }"""  should matchJson"""{ "a": true }"""
+      json"""{ "a": false }""" should matchJson"""{ "a": false }"""
+      json"""{ "a": null }"""  should matchJson"""{ "a": null }"""
+      json"""{ "a": 123 }"""   should matchJson"""{ "a": 123 }"""
+      json"""{ "a": "x" }"""   should matchJson"""{ "a": "x" }"""
+      json"""{ "a": [1] }"""   should matchJson"""{ "a": [1] }"""
+      json"""{ "a": { "b": 1 } }"""   should matchJson"""{ "a": { "b": 1 } }"""
+    }
+
+    "fail" in {
+      json"""{ "a": true }"""  shouldNot matchJson"""{ "a": 1 }"""
     }
   }
 }
