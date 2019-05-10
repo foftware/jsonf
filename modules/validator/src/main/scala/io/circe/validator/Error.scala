909d1df4ea3959d3bd37aca5020e1436cb8c63ec
Marek Kidon <marek.kidon@ceai.io>
2019-05-10 20:46:53 +0200

    Added header to all files

--- modules/validator/src/main/scala/io/circe/validator/Error.scala
+++ modules/validator/src/main/scala/io/circe/validator/Error.scala
@@ -1,3 +1,7 @@
+// Copyright (c) 2019 Marek Kidoň and František Kocun
+// This software is licensed under the MIT License (MIT).
+// For more information see LICENSE or https://opensource.org/licenses/MIT
+
 package io.circe.validator
 
 import cats.kernel.Eq
